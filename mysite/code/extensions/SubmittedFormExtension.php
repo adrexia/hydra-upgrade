4669964d52c87f5ffe97cd3f39dd219dfed96e92
Naomi Guyer <naomi@silverstripe.com>
2015-02-08 15:50:37 +1300

    User form display improvements

---
+++ mysite/code/extensions/SubmittedFormExtension.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Makes user form submissions more human readable
+ */
+class SubmittedFormExtension extends DataExtension {
+
+
+	public function Title() {
+		$date = SS_Datetime::create();
+		$date->setValue($this->owner->Created);
+		$dateReadable = $date->Format('D d M Y') .' at '. $date->Format('g:ia');
+		if($this->owner->SubmittedBy()) {
+			return '#' . $this->owner->ID .' Submitted by '. $this->owner->SubmittedBy()->getTitle() . ' on ' . $dateReadable;
+		} else {
+			return '#' . $this->owner->ID .' Submitted by unknown at ' . $dateReadable;
+		}
+	}
+
+}
---
+++ mysite/code/extensions/UserDefinedFormExtension.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Override display gridfield to provide more data
+ */
+
+class UserDefinedFormExtension extends DataExtension {
+
+	public function updateCMSFields(FieldList $fields) {
+		$submissions = $fields->dataFieldByName('Submissions');
+
+		$config = $submissions->getConfig();
+
+		$config->getComponentByType('GridFieldDataColumns')->setDisplayFields(
+			array(
+				'ID'=>'ID',
+				'SubmittedBy.Title'=>'Submitted By',
+				'Created'=>'Created'
+			)
+		);
+	}
+}
